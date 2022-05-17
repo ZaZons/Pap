d2a2ad03d4924a82c929a8cc7f57ec8f457e0ba4
ZaZons <tiago.ogomes.2004@gmail.com>
2022-05-17 16:47:19 +0100

    notification test

--- app/src/main/java/com/example/mplayer/QuackWorker.java
+++ app/src/main/java/com/example/mplayer/QuackWorker.java
@@ -1,14 +1,19 @@
-/*package com.example.mplayer;
+package com.example.mplayer;
+
+import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.work.Worker;
+import androidx.work.WorkerParameters;
 
 public class QuackWorker extends Worker {
+    public QuackWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
     @NonNull
     @Override
     public Result doWork() {
-        setRunInForeground(false);
         return null;
     }
 }
-*/
